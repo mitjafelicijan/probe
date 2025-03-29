f3926497cf91d6ae1345f36627cc5ecb57ddf316
Mitja Felicijan <mitja.felicijan@gmail.com>
2025-03-29 01:41:12 +0100

    Added Luajit experiments

--- c-luajit/fibonacci.c
+++ c-luajit/fibonacci.c
@@ -3,12 +3,14 @@
 #include <lauxlib.h>
 
 int main(int argc, char **argv) {
-    lua_State *L = luaL_newstate();
-    luaL_openlibs(L);
-    if (luaL_loadfile(L, "fibonacci.luac") || lua_pcall(L, 0, 0, 0)) {
-        fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
-        return 1;
-    }
-    lua_close(L);
-    return 0;
+	lua_State *L = luaL_newstate();
+	luaL_openlibs(L);
+
+	if (luaL_loadfile(L, "fibonacci.luac") || lua_pcall(L, 0, 0, 0)) {
+		fprintf(stderr, "Error: %s\n", lua_tostring(L, -1));
+		return 1;
+	}
+	
+	lua_close(L);
+	return 0;
 }
